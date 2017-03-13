d2c6d1237b2cc0c07975df8d56ff87de9312539d
Cameron Houser <cameronjhouser@gmail.com>
2017-03-13 14:44:43 -0400

    connected firebase, need to populate data on shopping list now

---
+++ src/app/Listings.ts
@@ -0,0 +1,12 @@
+export interface Listings{
+    $key?: string;
+    bedrooms?: string;
+    city?: string;
+    image?: string;
+    owner?: string;
+    path?: string;
+    price?: string;
+    title?:string;
+    type?: string;
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Component, OnInit } from '@angular/core';
+import {FirebaseService} from './services/firebase.services';
+import {Listings} from './Listings';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [FirebaseService]
 })
-export class AppComponent {
-  title = 'app works!';
-  items:FirebaseListObservable<any[]>;
-  constructor(af: AngularFire) {
-    this.items = af.database.list('items');
+export class AppComponent implements OnInit {
+  listings:Listings[];
+  constructor(private _firebaseService:FirebaseService) {
+  }
+
+  ngOnInit(){
+    this._firebaseService.getListings().subscribe(listings => { 
+      this.listings = listings;
+    });
   }
 }
