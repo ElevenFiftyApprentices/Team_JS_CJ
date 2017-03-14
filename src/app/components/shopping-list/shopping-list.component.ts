743146ecb1a148289e052b4f2f151d0b42cb3994
Jona Hyun <jona.hyun@gmail.com>
2017-03-14 12:54:56 -0400

    added styles to shopping list and updated shopping list component

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {FirebaseService} from '../../services/firebase.services';
+import {Listings} from '../../Listings';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
+  providers: [FirebaseService]
 })
 export class ShoppingListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  listings:Listings[];
+  constructor(private _firebaseService:FirebaseService) {
   }
 
+  ngOnInit(){
+    this._firebaseService.getListings().subscribe(listings => { 
+      this.listings = listings;
+    });
+  }
 }
+
