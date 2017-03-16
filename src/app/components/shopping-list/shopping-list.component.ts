b51e3b5da4b66b05ddb0cfb0aec5f514ffc23460
Cameron Houser <cameronjhouser@gmail.com>
2017-03-16 00:28:27 -0400

    item sort working, the sort list is populating repeated options - TODO

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FirebaseService} from '../../services/firebase.services';
 import {Listings} from '../../Listings';
+import {Category} from '../../Category';
 
 @Component({
   selector: 'app-shopping-list',
@@ -10,11 +11,26 @@ import {Listings} from '../../Listings';
 })
 export class ShoppingListComponent implements OnInit {
   listings:Listings[];
+  categories:Category[];
+  appState: string;
+  activeKey: string;
+
   constructor(private _firebaseService:FirebaseService) {
   }
 
   ngOnInit(){
     this._firebaseService.getListings().subscribe(listings => { 
+      this.listings = listings;
+    });
+
+    this._firebaseService.getCategories().subscribe(categories => {
+      this.categories = categories;
+    });
+  }
+
+
+  filterCategory(city){
+    this._firebaseService.getListings(city).subscribe(listings => {
       this.listings = listings;
     });
   }
